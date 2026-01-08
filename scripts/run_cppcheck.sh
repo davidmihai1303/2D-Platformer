97e3c9623f9240795858495dbe71dc675c1bbe0c
David Mihailescu <davidmihai0313@gmail.com>
2026-01-09 00:43:06 +0200

    fixes for tmxlite

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -5,6 +5,7 @@ cppcheck --enable=all \
     --project="${BUILD_DIR:-build}"/compile_commands.json \
     -i"${BUILD_DIR:-build}" --suppress="*:${BUILD_DIR:-build}/*" \
     -i"${EXT_DIR:-ext}" --suppress="*:${EXT_DIR:-ext}/*" \
+    -i"${EXT_DIR:-external}" --suppress="*:${EXT_DIR:-external}/*" \
     --suppress=missingIncludeSystem \
     --suppress=unmatchedSuppression \
     --suppress=useStlAlgorithm \
